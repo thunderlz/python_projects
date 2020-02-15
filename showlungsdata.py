47c36df92ed5e7069a7bd384bd60abd4bfc184b9
雷震 <leizhen@Leizs-iMac.local>
2020-02-15 21:27:49 +0800

    modify showlungsdata.py

--- showlungsdata.py
+++ showlungsdata.py
@@ -1,13 +1,9 @@
 import pandas as pd
 from sqlalchemy import create_engine
-#import matplotlib.pyplot as plt
 
+#import matplotlib.pyplot as plt
 engine=create_engine('mysql+pymysql://root:751982leizhen@192.168.31.200:3306/lungsdata')
 #engine=create_engine('mysql+pymysql://root:751982leizhen@192.168.31.200:3306/lungsdata')
-
 #df=pd.read_sql('select * from jsondata order by time desc limit 1',con=engine)
 df=pd.read_sql('select * from chinaday',con=engine)
-
 print(df)
-
-#df[['deadRate,healRate']].plot()
