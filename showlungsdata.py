6e13439842e94a88d87b86a774cd85660fe5b39b
雷震 <leizhen@Leizs-iMac.local>
2020-02-16 08:49:28 +0800

    m showfile

--- showlungsdata.py
+++ showlungsdata.py
@@ -8,5 +8,6 @@ engine=create_engine('mysql+pymysql://root:751982leizhen@192.168.31.200:3306/lun
 df=pd.read_sql('select * from chinaday',con=engine)
 print(df)
 dfjson=pd.read_sql('select * from jsondata',con=engine)
-print(dfjson.iloc[-1:,1])
-#add a sss
+querytime=dfjson.iat[-1,1]
+print('获取时间：'+querytime[0:4]+'年'+querytime[4:6]+'月'+querytime[6:8]+'日'+querytime[8:10]+'时')
+
