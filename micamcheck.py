4d144d351a08cdd373a920c0e9292bdd2fdb31de
leizhen <thunderlz@163.com>
2020-02-19 23:15:17 +0800

    修改了micamcheck.py

--- micamcheck.py
+++ micamcheck.py
@@ -3,10 +3,10 @@ import glob
 from datetime import datetime
 from datetime import timedelta
 import pandas as pd
-diskpath='/Volumes/micamsdir/xiaomi_camera_videos'
+diskpath='/mnt/data1/micam/xiaomi_camera_videos'
 with open(diskpath+'/log.txt','at') as logf:
     logf.write('检查时间：'+datetime.now().strftime('%Y年%m月%d日%H时%M分%S秒\n'))
-    micams=glob.glob(diskpath+'/*')
+    micams=glob.glob(diskpath+'/5ce50*')
     logf.write('一共有{}个摄像头数据\n'.format(len(micams)))
     for micam in micams:
         mdic={}
@@ -17,7 +17,7 @@ with open(diskpath+'/log.txt','at') as logf:
         start=(datetime.now()-timedelta(6)).strftime('%Y%m%d')
         keys=[h.strftime('%Y%m%d%H') for h in pd.date_range(start=start,periods=120,freq='H')]
         for key in keys:
-            print(' {}有{}个文件'.format(key,len(mdic[key])))
+      #      print(' {}有{}个文件'.format(key,len(mdic[key])))
             if len(mdic[key]) != 60:
                 logf.write('过去5天中，{}异常!!!!!!!!!!!!!\n'.format(key))
 
