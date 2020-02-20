84262ff792948b6d5fdca99e7f2a4a8ea0b0c8f8
雷震 <leizhen@Leizs-iMac.local>
2020-02-20 18:10:36 +0800

    修改了micamcheck,py，加入了视频的时间跨度

--- micamcheck.py
+++ micamcheck.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from datetime import timedelta
 import pandas as pd
 diskpath='/mnt/data1/micam/xiaomi_camera_videos'
+# diskpath='/Volumes/micamsdir/xiaomi_camera_videos'
 with open(diskpath+'/log.txt','at') as logf:
     logf.write('检查时间：'+datetime.now().strftime('%Y年%m月%d日%H时%M分%S秒\n'))
     micams=glob.glob(diskpath+'/5ce50*')
@@ -11,9 +12,11 @@ with open(diskpath+'/log.txt','at') as logf:
     for micam in micams:
         mdic={}
         dirs=glob.glob(micam+'/2020*')
+        dirs.sort()
         for dir in dirs:
             mdic[dir.split('/')[-1]]=glob.glob(dir+'/*.mp4')
-        logf.write('摄像头{}有{}个文件夹，{}个视频\n'.format(micam.split('/')[-1],len(mdic),sum([len(mdic[key]) for key in mdic.keys()])))
+        logf.write('摄像头{}有{}个文件夹，{}个视频，时间从{}到{}\n'.format(micam.split('/')[-1],len(mdic),sum([len(mdic[key]) for key in mdic.keys()]),
+                   dirs[0].split('/')[-1],dirs[-1].split('/')[-1]))
         start=(datetime.now()-timedelta(6)).strftime('%Y%m%d')
         keys=[h.strftime('%Y%m%d%H') for h in pd.date_range(start=start,periods=120,freq='H')]
         for key in keys:
