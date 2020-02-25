e567d53dcc5739fb35afcc409b6a97a030be1437
leizhen <thunderlz@163.com>
2020-02-25 15:54:08 +0800

    修改了lungsdata.py

--- lungsdata.py
+++ lungsdata.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 from sqlalchemy import create_engine
-engine=create_engine('mysql+pymysql://root:751982leizhen@192.168.31.200:3306/lungsdata')
+engine=create_engine('mysql+pymysql://root:751982leizhen@localhost:3306/lungsdata')
 #获取数据
 url='https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5&callback=jQuery34106118062347481377_1580545627633&_=1580545627634'
 headers={'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
