84a47b0378deeae593aad156bbcd9b88b394f136
leizhen <thunderlz@163.com>
2020-04-03 23:54:35 +0800

    lungsdata

--- lungsdata.py
+++ lungsdata.py
@@ -12,6 +12,8 @@ engine = create_engine(
     'mysql+pymysql://root:751982leizhen@localhost:3306/lungsdata')
 # 获取数据
 url = 'https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5&callback=jQuery34106118062347481377_1580545627633&_=1580545627634'
+# url='https://api.inews.qq.com/newsqa/v1/automation/modules/list'
+# data={'modules' : 'FAutoGlobalStatis,FAutoContinentStatis,FAutoGlobalDailyList,FAutoCountryWeekCompRank,FAutoCountryConfirmAdd,FAutoContinentConfirmStatis,FAutoConfirmMillionRankList'}
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
            }
 
@@ -19,17 +21,17 @@ session = requests.session()
 html = session.get(url, headers=headers)
 # 解析数据
 bsobj = BeautifulSoup(html.content, 'lxml')
-data = json.loads(json.loads(re.findall('\{.*\}', bsobj.p.string)[0])['data'])
+data = json.loads(re.findall('\{.*\}', bsobj.p.string)[0])
 # 获取国际的数据
 url_global = 'https://api.inews.qq.com/newsqa/v1/automation/modules/list?modules=FAutoGlobalStatis,FAutoContinentStatis,FAutoGlobalDailyList,FAutoCountryWeekCompRank,FAutoCountryConfirmAdd'
-html_global = session.get(url_global, headers=headers)
+html_global = session.post(url_global, headers=headers)
 bsobj_global = BeautifulSoup(html_global.content, 'lxml')
 
 data_global = json.loads(re.findall('\{.*\}', bsobj_global.p.string)[0])
 # print(data_global)
 # print(data)
-
-engine.execute('insert into jsondata values(time=%s,jsondata=%s)',
-               (datetime.now().strftime('%Y%m%d%H%M%S')),str(data))
-engine.execute('insert into jsondataglobal values(time=%s,jsondataglobal=%s)',
-               (datetime.now().strftime('%Y%m%d%H%M%S')),str(data_global))
+# print((datetime.now().strftime('%Y%m%d%H%M%S')))
+engine.execute('insert into jsondata values(%s,%s)',
+               str(data),datetime.now().strftime('%Y%m%d%H%M%S'))
+engine.execute('insert into jsondataglobal values(%s,%s)',
+               datetime.now().strftime('%Y%m%d%H%M%S'),str(data_global))
