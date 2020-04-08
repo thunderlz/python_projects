7879d0f9e7dcfde23a2bdc53cb04ed7af43720a6
雷震 <leizhen@Leizs-MacBook-Pro-16.local>
2020-04-08 21:16:20 +0800

    enter

--- socketdemo/socket_server.py
+++ socketdemo/socket_server.py
@@ -5,6 +5,8 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 9990))
 s.listen(5)
 print('Waiting for connection...')
+
+
 def tcplink(sock, addr):
     print('Accept new connection from %s:%s...' % addr)
     sock.send(b'Welcome!')
@@ -14,8 +16,11 @@ def tcplink(sock, addr):
         if not data or data.decode('utf-8') == 'exit':
             break
         sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
+        print(addr+':'+data.decode('utf-8'))
     sock.close()
     print('Connection from %s:%s closed.' % addr)
+
+
 while True:
     # 接受一个新连接:
     sock, addr = s.accept()
