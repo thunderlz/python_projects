021d5e3d8ce7b553da95344c7776dce1be91916f
雷震 <leizhen@Leizs-MacBook-Pro-16.local>
2020-04-08 21:51:39 +0800

    client

--- socketdemo/socket_client.py
+++ socketdemo/socket_client.py
@@ -1,11 +1,12 @@
 import socket
+import getpass
 import sys
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 建立连接:
 s.connect((sys.argv[1], 9990))
 # 接收欢迎消息:
 print(s.recv(1024).decode('utf-8'))
-for data in [b'Michael', b'Tracy', b'Sarah',b'fuck','utf-8'.encode('utf-8')]:
+for data in [getpass.getuser().encode('utf-8'), socket.gethostname().encode('utf-8'), 'end'.encode('utf-8')]:
     # 发送数据:
     s.send(data)
     print(s.recv(1024).decode('utf-8'))
